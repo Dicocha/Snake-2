b8d5cdb8a91ba1d698a30a2083be2d1c85e6ee3d
Dicocha <44475882+Dicocha@users.noreply.github.com>
2022-05-13 16:48:21 -0600

    Add files via upload

--- Juego.py
+++ Juego.py
@@ -1,53 +1,40 @@
 # Este programa se basa en https://www.geeksforgeeks.org/snake-game-in-python-using-pygame-module/ 
 # Librerias
+from time import sleep
 import pygame, random, pymysql
 from pygame.locals import *
-
-# Iniciar pygame
-pygame.init()
-
-# Variables de tamano
-Win_x = 720
-Win_y = 480
-Bottom_x = Win_x/4
-
-# Configuracion de ventanas
-# pygame.display.
-
-# Variables del personaje
-VelocidadPersonaje = 15
-PosicionPersonaje  = [100, 50]
-CuerpoPersonaje = [[100, 50], [85, 50], [75, 50], [65, 50]]
-
-# Colores
-Negro = pygame.Color(0, 0, 0)
-Blanco = pygame.Color(255, 255, 255)
-color_active = pygame.Color(Blanco)
-color_passive = pygame.Color(Blanco)
-color = color_passive
-
-# FPS
-FPS = pygame.time.Clock()
-
-# Variables del alimento
-PosicionFruta = [random.randrange(1, (Win_x//10)) * 10, random.randrange(1, (Win_y//10)) * 10]
-SpawnFruta = True
-
-# Esto es para iniciar a la derecha 
-Direccion = 'RIGHT'
-change_to = Direccion
-
-# Fuente de letras
-font = pygame.font.Font("Recursos/Jackpot.ttf", 50)
-fontbot = pygame.font.Font("Recursos/Jackpot.ttf", 40)
-font_base = pygame.font.Font("Recursos/Jackpot.ttf", 30)
-font_P = pygame.font.Font("Recursos/Jackpot.ttf", 20)
 
 # Ventanas
 # Ciclo principal para el juego
 def main():
-	global click
+	# Iniciar pygame
+	pygame.init()
+
+	# Variables de tamano
+	global Win_x, Win_y, screen
+	Win_x = 720
+	Win_y = 480
+	Bottom_x = Win_x/4
+	# Configuracion de ventanas
+	screen = pygame.display.set_mode((Win_x, Win_y), pygame.NOFRAME)
+
+	# Colores
+	global Negro, Blanco
+	Negro = pygame.Color(0, 0, 0)
+	Blanco = pygame.Color(255, 255, 255)
+
+
+	# FPS
+	global FPS
+	FPS = pygame.time.Clock()
+
+	# Fuente de letras
+	global font, fontbot
+	font = pygame.font.Font("Recursos/Jackpot.ttf", 50)
+	fontbot = pygame.font.Font("Recursos/Jackpot.ttf", 40)
+
 	click = False
+	
 	while True:
 		# Este ciclo es para cerrar el programa
 		# handling key events
@@ -62,9 +49,6 @@ def main():
 			if event.type == MOUSEBUTTONDOWN:
 				if event.button == 1:
 					click = True
-
-		# Objetos para la pantalla principal 
-		screen = pygame.display.set_mode((Win_x, Win_y))
 
 		# Fondo de pantalla
 		screen.fill(Negro)
@@ -85,19 +69,22 @@ def main():
 		start_text('Puntaje', fontbot, Blanco, screen, 210, 225)
 		start_text('Salir', fontbot, Blanco, screen, 260, 325)
 
-		# Control
+		# Controles
 		mx, my = pygame.mouse.get_pos()
 
 		# Acciones de los botones
 		if button_1.collidepoint(mx, my):
+			start_text('Inicio', fontbot, (130,130,130), screen, 265, 129)
 			if click:
-				Game(change_to, Direccion, PosicionPersonaje, CuerpoPersonaje, PosicionFruta, VelocidadPersonaje, SpawnFruta)
+				Game()
 
 		if button_2.collidepoint(mx, my):
+			start_text('Puntaje', fontbot, (130,130,130), screen, 210, 225)
 			if click:
 				TablaPosiciones()
 
 		if button_3.collidepoint(mx,my):
+			start_text('Salir', fontbot, (130,130,130), screen, 260, 325)
 			if click:
 				pygame.quit()
 				exit()
@@ -108,10 +95,34 @@ def main():
 		FPS.tick(60)
 
 # Modulo del juego
-def Game(change_to, Direccion, PosicionPersonaje, CuerpoPersonaje, PosicionFruta, VelocidadPersonaje, SpawnFruta):
+def Game():
+	global Negro, Blanco, FPS
+	Rojo = pygame.Color(255,0,0)
+
+	# Variables del personaje
+	VelocidadPersonaje = 15
+	PosicionPersonaje  = [100, 50]
+	CuerpoPersonaje = [[100, 50], [85, 50], [75, 50], [65, 50]]
+
+	# Variables del alimento
+	PosicionFruta = [random.randrange(1, (Win_x//10)) * 10, random.randrange(1, (Win_y//10)) * 10]
+	PosicionPwrUp = [random.randrange(1, (Win_x//10)) * 10, random.randrange(1, (Win_y//10)) * 10]
+	SpawnPwrUp = True
+	SpawnFruta = True
+
+	# Fuente
+	font_P = pygame.font.Font("Recursos/Jackpot.ttf", 20)
+
 	# Puntuacion
-	global Puntuacion 
 	Puntuacion = 0
+
+	# Esto es para iniciar a la derecha 
+	Direccion = 'RIGHT'
+	change_to = Direccion
+
+	#Musica
+	music = pygame.mixer.Sound("")
+	music.play()
 
 	while True:
 		# handling key events
@@ -167,6 +178,7 @@ def Game(change_to, Direccion, PosicionPersonaje, CuerpoPersonaje, PosicionFruta
 		CuerpoPersonaje.insert(0, list(PosicionPersonaje))
 		if PosicionPersonaje[0] == PosicionFruta[0] and PosicionPersonaje[1] == PosicionFruta[1]:
 			Puntuacion +=  1
+			VelocidadPersonaje +=1
 			SpawnFruta = False
 		else:
 			CuerpoPersonaje.pop()
@@ -176,27 +188,46 @@ def Game(change_to, Direccion, PosicionPersonaje, CuerpoPersonaje, PosicionFruta
 
 		SpawnFruta = True
 
-		screen_game = pygame.display.set_mode((Win_x, Win_y))
-		screen_game.fill(Negro)
+		# 
+		#Tiempo = sleep(1.0)
+		#if Tiempo == 0:
+			# PowerUp
+		if PosicionPersonaje[0] == PosicionPwrUp[0] and PosicionPersonaje[1] == PosicionPwrUp[1]:
+			VelocidadPersonaje = 15
+			Puntuacion *= 2
+			SpawnPwrUp = False
+				
+		if not SpawnPwrUp:
+			PosicionPwrUp = [random.randrange(1, (Win_x//10)) * 10, random.randrange(1, (Win_y//10)) * 10]
+
+		SpawnPwrUp = True
+
+		screen.fill(Negro)
+		Borde = pygame.Rect(0, 0, Win_x, Win_y)
+		pygame.draw.rect(screen, Blanco, Borde, width=2)
 
 		for pos in CuerpoPersonaje:
-			pygame.draw.rect(screen_game, Blanco, pygame.Rect(pos[0], pos[1], 15, 15))
-		pygame.draw.rect(screen_game, Blanco, pygame.Rect(PosicionFruta[0], PosicionFruta[1], 15, 15))
+			pygame.draw.rect(screen, Blanco, pygame.Rect(pos[0], pos[1], 15, 15))
+		pygame.draw.rect(screen, Blanco, pygame.Rect(PosicionFruta[0], PosicionFruta[1], 15, 15))
+		pygame.draw.rect(screen, Rojo, pygame.Rect(PosicionPwrUp[0], PosicionPwrUp[1], 15, 15))
 
 		# Game Over conditions
 		if PosicionPersonaje[0] < 0 or PosicionPersonaje[0] > Win_x-10:
-			game_over()
+			music.stop()
+			game_over(Puntuacion)
 
 		if PosicionPersonaje[1] < 0 or PosicionPersonaje[1] > Win_y-10:
-			game_over()
+			music.stop()
+			game_over(Puntuacion)
 
 		# Touching the snake body
 		for block in CuerpoPersonaje[1:]:
 			if PosicionPersonaje[0] == block[0] and PosicionPersonaje[1] == block[1]:
-				game_over()
+				music.stop()
+				game_over(Puntuacion)
 	
 		# displaying score countinuously
-		start_text('Puntuacion: ' + str(Puntuacion), font_P, Blanco, screen_game, 250, 2)
+		start_text('Puntuacion: ' + str(Puntuacion), font_P, Blanco, screen, 250, 2)
 
 		# Refresh game screen
 		pygame.display.update()
@@ -206,6 +237,7 @@ def Game(change_to, Direccion, PosicionPersonaje, CuerpoPersonaje, PosicionFruta
 
 # Modulo de las puntuaciones
 def TablaPosiciones():
+	click = False
 	while True:
 		# Este ciclo es para cerrar el programa
 		# handling key events
@@ -220,34 +252,35 @@ def TablaPosiciones():
 				if event.button == 1:
 					click = True
 
-		screen_Tabla = pygame.display.set_mode((Win_x, Win_y))
-		screen_Tabla.fill(Negro)
+		screen.fill(Negro)
 		Borde = pygame.Rect(0, 0, Win_x, Win_y)
-		pygame.draw.rect(screen_Tabla, Blanco, Borde, width=2)
-
-		# Control
-		mx, my = pygame.mouse.get_pos()
+		pygame.draw.rect(screen, Blanco, Borde, width=2)
 
 		# Botones 
 		Regresar = pygame.Rect(50, 350, 370, 75)
 		Salir = pygame.Rect(460, 350, 220, 75)
-		pygame.draw.rect(screen_Tabla, Blanco, Regresar, width=2)
-		pygame.draw.rect(screen_Tabla, Blanco, Salir, width=2)
+		pygame.draw.rect(screen, Blanco, Regresar, width=2)
+		pygame.draw.rect(screen, Blanco, Salir, width=2)
 
 		# Text
-		start_text('Puntuaciones', font, Blanco, screen_Tabla, 70, 20)
-		start_text('Regresar', fontbot, Blanco, screen_Tabla, 63, 344)
-		start_text('Salir', fontbot, Blanco, screen_Tabla, 477, 344)
+		start_text('Los 3 mejores', font, Blanco, screen, 60, 20)
+		start_text('Regresar', fontbot, Blanco, screen, 63, 344)
+		start_text('Salir', fontbot, Blanco, screen, 477, 344)
 
 		# Llamar la funcion conexion
-		ConexionConsulta(screen_Tabla)
+		ConexionConsulta()
+
+		# Controles
+		mx, my = pygame.mouse.get_pos()
 
 		# Acciones de los botones
 		if Regresar.collidepoint(mx, my):
+			start_text('Regresar', fontbot, (130,130,130), screen, 63, 344)
 			if click:
 				main()
 
 		if Salir.collidepoint(mx, my):
+			start_text('Salir', fontbot, (130,130,130), screen, 477, 344)
 			if click:
 				pygame.quit()
 				exit()
@@ -257,7 +290,8 @@ def TablaPosiciones():
 
 # Conexion a la base de datos 
 # Modulo para consultar
-def ConexionConsulta(screen_Tabla):
+def ConexionConsulta():
+	font_base = pygame.font.Font("Recursos/Jackpot.ttf", 30)
 	# Conexion con la base de datos
 	conn = pymysql.connect( 
 		host='localhost', 
@@ -270,33 +304,16 @@ def ConexionConsulta(screen_Tabla):
 	iy = 120
 
 	cur = conn.cursor()
-	cur.execute("SELECT * FROM `Snake` ORDER BY `Puntuacion` DESC")
-	for Nombre, Puntos in cur.fetchall():
-		for i,j in Nombre, Puntos:
-			start_text(i+'.....'+j,font_base, Blanco, screen_Tabla, ix, iy)
-			iy += 50
-
-#	cur = conn.cursor()
-#	cur.execute("SELECT `Nombres` FROM `Snake`")
-#	Nombres = [Nombre[0] for Nombre in cur.fetchall()]
-#	for i in Nombres:
-#		start_text(i,font_base, Blanco, screen_Tabla, ix, iy)
-#		iy += 50
-
-#	jx = 470
-#	jy = 120
-
-#	cur = conn.cursor()
-#	cur.execute("SELECT `Puntuacion` FROM `Snake`")
-#	Puntuacion = [Puntos[0] for Puntos in cur.fetchall()]
-#	for j in Puntuacion:
-#		start_text(str(j), font_base, Blanco, screen_Tabla, jx, jy)
-#		jy += 50
+	cur.execute("SELECT concat(`Nombres`,'                            ',`Puntuacion`) from Snake order by `Puntuacion` desc limit 3")
+	Nombres = [Nombre[0] for Nombre in cur.fetchall()]
+	for i in Nombres:
+		start_text(i,font_base, Blanco, screen, ix, iy)
+		iy += 50
 
 	conn.close()
 
 # Modulo para insertas datos
-def ConexionInsertar():
+def ConexionInsertar(user_text, Puntuacion, screen, Mensaje):
 	# Conexion con la base de datos
 	conn = pymysql.connect( 
 		host='localhost', 
@@ -305,20 +322,33 @@ def ConexionInsertar():
 		db='Proyectos', 
 	)
 
+	# Eliminar el registro con el mismo nombre
+	cur = conn.cursor()
+	sql = "DELETE FROM `Snake` WHERE `Nombres` = %s"
+	val = (user_text);
+
+	cur.execute(sql, val)
+	conn.commit()
+
+	# Insertar el registro
 	cur = conn.cursor()
 	sql = "INSERT INTO `Snake`(`Nombres`, `Puntuacion`) VALUES (%s,%s)"
 	val = (user_text, Puntuacion);
 
 	cur.execute(sql, val)
 	conn.commit()
-	print(cur.rowcount, "record inserted.")
+
 	conn.close()
 
 # Mecanicas
 # Modulo de game over
-def game_over():
-	global user_text
+def game_over(Puntuacion):
+	font = pygame.font.Font("Recursos/Jackpot.ttf", 30)
+	font_ins = pygame.font.Font("Recursos/Jackpot.ttf", 10)
+	click = False
 	user_text = ''
+	Mensaje = ''
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == MOUSEBUTTONDOWN:
@@ -331,56 +361,68 @@ def game_over():
 					user_text = user_text[:-1]
 				else:
 					user_text += event.unicode
+				# Limit characters
+					if text_surface.get_width() > Entrada.w - 15:
+						user_text = user_text[:-1]
 
-		screen_GameOver = pygame.display.set_mode((Win_x, Win_y))
-		screen_GameOver.fill(Negro)
+		screen.fill(Negro)
 		Borde = pygame.Rect(0, 0, Win_x, Win_y)
-		pygame.draw.rect(screen_GameOver, Blanco, Borde, width=2)
+		pygame.draw.rect(screen, Blanco, Borde, width=2)
 		
 		# Mostrando la puntuacion
-		PuntuacionGameOver = font_base.render('Tu puntuacion es: ' + str(Puntuacion), True, Blanco)
+		PuntuacionGameOver = font.render('Tu puntuacion es: ' + str(Puntuacion), True, Blanco)
 		game_over_rect = PuntuacionGameOver.get_rect()
 		
 		# Configuracion de la puntuacion
-		game_over_rect.midtop = (380, 20)
+		game_over_rect.midtop = (350, 20)
 		
 		# Mostrar la puntuacion
-		screen_GameOver.blit(PuntuacionGameOver, game_over_rect)
+		screen.blit(PuntuacionGameOver, game_over_rect)
 
 		# Botones
-		Entrada = pygame.Rect(320, 120, 300, 60)
+		Entrada = pygame.Rect(320, 120, 150, 70)
 		MenuPrincipal = pygame.Rect(160, 225, 430, 60)
 		Guardar = pygame.Rect(220, 300, 300, 60)
 		Salir = pygame.Rect(220, 375, 300, 60)
 
-		pygame.draw.rect(screen_GameOver, Blanco, Entrada, width=2)
-		pygame.draw.rect(screen_GameOver, Blanco, MenuPrincipal, width=2)
-		pygame.draw.rect(screen_GameOver, Blanco, Guardar, width=2)
-		pygame.draw.rect(screen_GameOver, Blanco, Salir, width=2)
+		pygame.draw.rect(screen, Blanco, Entrada, width=2)
+		pygame.draw.rect(screen, Blanco, MenuPrincipal, width=2)
+		pygame.draw.rect(screen, Blanco, Guardar, width=2)
+		pygame.draw.rect(screen, Blanco, Salir, width=2)
 
 		# Text
-		start_text('Nombre: ', font_base, Blanco, screen_GameOver, 70, 120)
-		start_text(user_text, font_base, Blanco, screen_GameOver, 330, 117)
-		start_text('Menu Principal', font_base, Blanco, screen_GameOver, 180, 225)
-		start_text('Guardar', font_base, Blanco, screen_GameOver, 250, 300)
-		start_text('Salir', font_base, Blanco, screen_GameOver, 295, 373)
+		text_surface = font.render(user_text, True, Blanco)
+		start_text('Nombre: ', font, Blanco, screen, 80, 120)
+		start_text('Menu Principal', font, Blanco, screen, 180, 225)
+		start_text('Guardar', font, Blanco, screen, 250, 300)
+		start_text('Salir', font, Blanco, screen, 295, 373)
+		start_text(Mensaje, font_ins, Blanco, screen, 500, 140)
 
-		# Control
+		# Posicion de la entrada
+		screen.blit(text_surface, (Entrada.x+5, Entrada.y+5))
+		Entrada.w = max(100, text_surface.get_width()+10)
+
+		# Controles
 		mx, my = pygame.mouse.get_pos()
 
 		# Acciones de los botones
 		if MenuPrincipal.collidepoint(mx, my):
+			start_text('Menu Principal', font, (130,130,130), screen, 180, 225)
 			if click:	
 				main()
 					
 		if Salir.collidepoint(mx, my):
+			start_text('Salir', font, (130,130,130), screen, 295, 373)
 			if click:
 				pygame.quit()
 				exit()
 
 		if Guardar.collidepoint(mx, my):
+			start_text('Guardar', font, (130,130,130), screen, 250, 300)
 			if click:
-				ConexionInsertar()
+				ConexionInsertar(user_text, Puntuacion, screen, Mensaje)
+				sleep(0.5)
+				Mensaje = 'Guardado!!'
 
 		click = False
 		pygame.display.update()
@@ -394,5 +436,6 @@ def start_text(text, font, color, surface, x, y):
     textrect.topleft = (x, y)
     surface.blit(textobj, textrect)
 
+# Ejecutador del programa
 if __name__ == "__main__" : 
     main()
